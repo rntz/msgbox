0ec0fe81d8c3778ffdbf25d3c06aff469b172376
Michael Arntzenius <daekharel@gmail.com>
2013-03-20 19:13:58 -0400

    lots work

--- peer.py
+++ peer.py
@@ -2,11 +2,15 @@
 from collections import deque, namedtuple
 import asyncore
 import bisect
+import errno
 import json
 import optparse
 import os, os.path
+import socket
 import struct
 import sys
+
+LISTEN_BACKLOG = 5
 
 # format in which packets are sent, using the struct module's format string.
 # this means "big-endian 4-byte unsigned integer".
@@ -38,16 +42,6 @@ def parse_config(options):
     return cfg
 
 
-# Node types - the types of nodes that can connect to us / we can connect to.
-NODE_UNKNOWN = None
-# A full peer; sends and receives messages.
-NODE_PEER = "peer"
-# An event sender; only sends messages.
-NODE_SENDER = "sender"
-
-NODE_TYPES = [NODE_PEER, NODE_SENDER]
-
-
 # The send-multiqueue
 #
 # Keeps track of data (split into chunks, but this is only a sender-side
@@ -67,13 +61,15 @@ class MultiQueue(object):
     def disconnect(self, dest):
         del self.queues[dest]
 
-    # schedules a chunk to be sent to each destination in `dests`. Returns a
-    # list of exactly those destinations in `dests` which previously had no data
-    # scheduled to be sent to them.
-    def add(self, chunk, dests):
+    # Schedules a list of chunks to be sent to each destination in `dests`
+    # (every chunk gets sent to every destination; first chunk in the list gets
+    # sent first). Returns a list of exactly those destinations in `dests` which
+    # previously had no data scheduled to be sent to them, but now do.
+    def enqueue(self, dests, chunks):
+        if not chunks: return []
         empty_dests = [d for d in dests if not self.queues[d]]
         for dest in dests:
-            self.queues[dest].add(chunk)
+            self.queues[dest].extend(chunks)
         return empty_dests
 
     # Tries to send as much data as possible to a given destination. `sender`
@@ -91,8 +87,8 @@ class MultiQueue(object):
 
         def __len__(self): return len(self.chunks)
 
-        def add(self, chunk):
-            self.chunks.append(chunk)
+        def extend(self, chunks):
+            self.chunks.extend(chunks)
 
         def send(self, sender):
             while self.chunks:
@@ -162,15 +158,22 @@ class VClock(Jsonable):
         return dict(self.times)
 
 
+# Node types - the types of nodes that can connect to us / we can connect to.
+NODE_UNKNOWN = None
+NODE_TYPES = "peer sender".split()
+globals().update({'NODE_' + t.upper(): t for t in NODE_TYPES})
+
 # Packet types - the types of packets that we can receive.
 #
 # Note: "packet" does NOT mean a TCP packet. we're using the term for our own
 # purposes. I wish I knew a better one.
-PACKET_INIT = "init"
-PACKET_QUIT = "quit"
-PACKET_MESSAGE = "message"
-PACKET_TYPES = [PACKET_INIT, PACKET_QUIT, PACKET_MESSAGE]
+#
+# TODO: do we need a "bye" message, given that we can call shutdown() on
+# sockets?
+PACKET_TYPES = "hello welcome uptodate bye message".split()
+globals().update({'PACKET_' + t.upper(): t for t in PACKET_TYPES})
 
+
 # A packet.
 class Packet(Jsonable):
     def __init__(self, dictionary):
@@ -189,17 +192,23 @@ class Packet(Jsonable):
 def PacketMessage(message):
     return Packet(type=PACKET_MESSAGE, message=message)
 
-def PacketInitPeer(peer_id, vclock):
-    return Packet(type=PACKET_INIT,
+def PacketHelloPeer(peer_id, vclock):
+    return Packet(type=PACKET_HELLO,
                   node_type=NODE_PEER,
                   peer_id=peer_id,
-                  vclock=vclock)
+                  vclock=vclock.copy())
 
-def PacketInitSender():
+def PacketHelloSender():
     return Packet(type=PACKET_INIT, node_type=NODE_SENDER)
 
-def PacketQuit():
-    return Packet(type=PACKET_QUIT)
+def PacketWelcome(peer_id, vclock):
+    return Packet(type=PACKET_WELCOME, peer_id=peer_id, vclock=vclock.copy())
+
+def PacketUptodate():
+    return Packet(type=PACKET_UPTODATE)
+
+def PacketBye():
+    return Packet(type=PACKET_BYE)
 
 # A message, consisting of a source identifier, a timestamp, and a JSON payload.
 class Message(Jsonable):
@@ -263,29 +272,54 @@ class MessageStore(Jsonable):
         return json
 
 
-# Handles an individual socket for PeerController
-class SockHandler(asyncore.dispatcher):
-    def __init__(self, controller):
-        super(SockHandler, self).__init__()
-        self.controller = controller
-        self.node_type = NODE_UNKNOWN
+# Handles the socket we listen on incoming connections for
+class ListenHandler(asyncore.dispatcher):
+    def __init__(self, sock, map, coro):
+        super(ListenHandler, self).__init__(sock=sock, map=map)
+        self.coro = coro
+
+    def handle_accept(self):
+        # TODO: should we try/catch here for EAGAIN/EWOULDBLOCK
+        pair = self.accept()
+        # handle_accept() shouldn't be called unless we can accept, I think?
+        assert pair is not None
+        sock, addr = pair
+        print 'Incoming connection from %s' % repr(addr) # TODO: remove
+        self.controller.handle_incoming(sock, addr)
+
+    def handle_close(self):
+        raise NotImplemented()  # FIXME
+
+    def readable(self): return False
+    def writable(self): return False
+
+
+# Handles the IO events on an individual socket for PeerController
+class ConnHandler(asyncore.dispatcher):
+    def __init__(self, sock=None, map=None, reader=None, writer=None):
+        super(ConnHandler, self).__init__(sock=sock, map=map)
         self.recvd = []
+        self.reader = reader
+        self.writer = writer
         # True if we're reading a packet's length header, False if we're reading
         # its body
         self.reading_header = True
         self.expecting = PACKET_LEN_BYTES
 
+    def readable(): return self.reader is not None
+    def writable(): return self.writer is not None
+
     def expect(self, n_bytes):
         assert self.expecting == 0
         self.expecting = n_bytes
 
+    # must be implemented by subclass
     def handle_write(self):
-        # FIXME: need to avoid waiting for writes unless we're a full peer.
-        # otherwise this assert will trip.
-        assert self.node_type == NODE_PEER
-        self.controller.handle_write(self)
+        assert self.writer is not None
+        self.writer()
 
     def handle_read(self):
+        assert self.reader is not None
         # is self.connected the right thing to loop on?
         while self.connected:
             assert self.expecting > 0
@@ -310,75 +344,148 @@ class SockHandler(asyncore.dispatcher):
             self.reading_header = False
             self.expect(packet_len)
         else:
-            self.handle_packet(chunk)
+            self.reader(Packet.from_json(json.loads(chunk)))
             self.reading_header = True
             self.expect(PACKET_LEN_BYTES)
 
-    def handle_packet(self, packet):
-        p = Packet.from_json(json.loads(packet))
-        self.controller.handle_packet(self, p)
-
 
-# an event handler that manages almost everything else
-class PeerController(object):
-    def __init__(self, peer_id, message_store):
-        self.peer_id = peer_id
-        self.message_store = message_store
+# deals with IO and shit
+# I don't really know what this class' responsibilities are
+# it does what it does
+class Peer(object):
+    def __init__(self, listen_socket, peer_id, vclock, message_store, remotes):
         self.queue = MultiQueue()
-        # maps from socket ids to their handlers
-        self.sockets = {}
+        self.sockets = set()
+        self.socket_map = {}    # for dispatchers
+        self.peers = set()
 
-    # **********************************************************************
-    # io_manager must satisfy the following interface:
-    #
-    # - io.id(sock) -> unique hashable identifier for sock
-    #
-    # - io.write(sock, msg) -> number of bytes written
-    #   Must not block.
-    #
-    # - io.read(sock) -> string
-    #   Must not block. Must return as much data as available.
-    #
-    # - io.close(sock)
-    #   Closes & shuts down sock.
+        self.peer_id = peer_id
+        self.vclock = vclock
+        self.message_store = message_store
+        self.remotes = remotes  # remotes to connect to
+        self.listen_socket = ListenSock(controller=self, sock=listen_socket)
 
-    def handle_packet(self, socket, packet):
-        if socket.node_type == NODE_UNKNOWN:
-            assert packet.type == PACKET_INIT
-            # Add the node in
-            self.handle_init(socket, packet)
-        elif socket.node_type in [NODE_PEER, NODE_SENDER]:
-            if packet.type == PACKET_MESSAGE:
-                self.handle_message(socket, packet.message)
-            elif packet.type == PACKET_QUIT:
-                self.handle_quit(socket)
-            else:
-                assert False    # malformed packet, TODO: handle error
+    def run(self):
+        raise NotImplemented()  # FIXME
+
+    def connect(self, sock):
+        assert sock.connected   # TODO: hackish, relies on dispatcher internals
+        self.sockets.add(sock)
+        self.queue.connect(sock)
+
+    def disconnect(self, sock):
+        # TODO: hackish b/c `sock.connected` internal to dispatcher
+        assert not sock.connected
+        assert sock not in self.peers
+        self.sockets.remove(sock)
+        self.queue.disconnect(sock)
+
+    def make_handler(self, sock, coro_maker, writable=False):
+        handler = ConnHandler(sock=sock, map=self.socket_map)
+        coro = coro_maker(handler)
+        coro.next()             # make sure calls to coro.send work
+        handler.reader = coro.send
+        if writable:
+            handler.writer = lambda: self.handle_write(handler)
+
+    # Schedules packets for sending to a list of sockets.
+    def send_many(self, socks, packets):
+        def serialize(packet):
+            data = json.dumps(packet.to_json())
+            header = struct.pack(PACKET_LEN_FMT, len(data))
+            return header + data
+        newly_active = self.queue.enqueue(socks, map(serialize, packets))
+        for sock in newly_active:
+            self.try_sending_to(sock)
+
+    def send_one(self, socks, packet):
+        return self.send_many(socks, [packet])
+
+    # handling incoming connections
+    def handle_incoming(self, sock, addr):
+        sock = self.make_handler(sock, self.incoming_reader)
+        self.connect(sock)
+
+    def incoming_reader(self, sock):
+        # TODO: handle unannounced shutdowns. probably need a try/catch block.
+
+        hello = yield
+        # TODO: couldn't the first packet (or ANY packet) be a BYE?
+        assert hello.type == PACKET_HELLO   # TODO: error handling
+        assert peer.node_type in NODE_TYPES # TODO: error handling
+        node_type = hello.node_type
+
+        # this code could be so much cleaner if `yield from` was available.
+        if node_type == NODE_PEER:
+            peer_id = sock.peer_id = hello.peer_id
+
+            # add a writer for the peer
+            sock.writer = lambda: self.handle_write(sock)
+
+            # send welcome response and updates for peer
+            msgs = self.updates_for(hello.vclock)
+            packets = ([PacketWelcome(self.peer_id, self.vclock)]
+                       + [PacketMessage(msg) for msg in msgs]
+                       + [PacketUptodate()])
+            self.send_many([sock], packets)
+
+            # now, accept messages from our peer
+            while True:
+                packet = yield
+                if packet.type == PACKET_BYE: break
+                # TODO: handle bad message types
+                assert packet.type == PACKET_MESSAGE
+                self.handle_message(packet.message, recvd_from=sock)
+
+            # got a bye from a peer
+            self.peers.remove(sock)
+
+        elif node_type == NODE_SENDER:
+            # just read messages from them.
+            while True:
+                packet = yield
+                if packet.type == PACKET_BYE: break
+                # TODO: handle bad message types
+                assert packet.type == PACKET_MESSAGE
+                self.handle_message(packet.message, recvd_from=sock)
+
         else:
             assert False        # unreachable case
 
-    def handle_init(self, socket, packet):
-        # TODO: handle malformed packet node types
-        assert packet.node_type in NODE_TYPES
-        socket.node_type = packet.node_type
-        if socket.node_type == NODE_PEER:
-            # if they're a full peer, we need to get them up to date
-            socket.peer_id = pkt.peer_id
-        # FIXME
-        raise NotImplemented()
+        # we got a bye.
+        sock.shutdown(socket.SHUT_RDWR)
+        sock.close()
+        self.disconnect(sock)
 
-    def handle_quit(self, socket):
-        pass                    # FIXME
+    def updates_for(self, vclock):
+        msgdict = self.message_store.messages_after_vclock(vclock)
+        for src, msgs in msgdict.iteritems():
+            for m in msgs: yield m
 
-    def handle_message(self, socket, message):
-        pass                    # FIXME
+    def handle_message(self, message, recvd_from=None):
+        raise NotImplemented()  # FIXME
 
-    def handle_write(self, socket):
-        assert socket.node_type == NODE_PEER
-        pass
+    def handle_write(self, sock):
+        self.try_sending_to(sock)
+
+    def try_sending_to(self, sock):
+        assert sock.node_type == NODE_PEER
+        def sender(chunk):
+            try:
+                sent = sock.send(chunk, socket.MSG_DONTWAIT)
+            except socket.error as (code, msg):
+                # catch errors due to non-blocking
+                if code not in [errno.EWOULDBLOCK, errno.EAGAIN]:
+                    raise
+                sent = 0
+            return sent
+        self.queue.send(sock, sender)
 
     # Handles a new connection.
     def handle_connect(self, sock): pass # FIXME
     def handle_disconnect(self, sock): pass # FIXME
 
     def shutdown(self): pass
+
+
+# Handles incoming connections
